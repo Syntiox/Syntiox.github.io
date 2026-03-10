c33509a578457b2cbeadb8a4dd27c7ee78b789cc
﻿ <shalukagimhan.ai@gmail.com>
2026-03-11 01:16:18 +0530

    Create data.js

---
+++ Member/ruk-zh4n/assets/js/data.js
@@ -0,0 +1,75 @@
+// ==========================================
+// 1. SOCIAL LINKS 
+// ==========================================
+export const socialLinks = [
+    { 
+        name: "GitHub", 
+        desc: "Coding & Mods",    
+        url: "https://github.com/ruk-zh4n", 
+        icon: "fab fa-github", 
+        color: "text-white",
+        size: "small" 
+    },  
+   
+ 
+    { 
+        name: "YouTube", 
+        desc: "Tutorials & Explanations", 
+        url: "https://www.youtube.com/@ruk-zh4n", 
+        icon: "fab fa-youtube", 
+        color: "text-red-600",
+        size: "wide" 
+    },        
+
+    { 
+    name: "Instagram", 
+    desc: "Post sharing",
+    url: "https://www.instagram.com/malindu2696", 
+    icon: "fab fa-instagram",
+    color: "text-white", 
+    background: "linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)",
+    className: "bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600",
+    size: "small"
+}
+];
+
+// ==========================================
+// 2. TECH STACK 
+// ==========================================
+export const techStack = [
+    { name: "JavaScript", icon: "fab fa-js", color: "text-yellow-400" }, 
+    { name: "Node.js", icon: "fab fa-node-js", color: "text-green-500" },
+    { name: "Python", icon: "fab fa-python", color: "text-blue-400" },
+    { name: "Metasploit", icon: "fas fa-bug", color: "text-red-400" },
+    { name: "Databases & Datasets", icon: "fas fa-database", color: "text-cyan-400" }, 
+    { name: "React", icon: "fab fa-react", color: "text-cyan-500" }, 
+    { name: "Bash / Shell", icon: "fas fa-terminal", color: "text-gray-300" }
+];
+
+// ==========================================
+// 3. PROJECTS 
+// ==========================================
+export const projects = [
+
+    {
+        title: "......",
+        category: "...",
+        date: "...",
+        desc: "..............",
+        url: "...",
+        color: "cyan"
+    },
+    
+
+    {
+        title: "...",
+        category: "...",
+        date: "...",
+        desc: "....",
+        url: "..",
+        color: "cyan"
+    }
+
+    
+     
+];
