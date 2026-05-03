fd698ec626ec3cc1ae82aefb7d28eac91c8da3c7
﻿ <shalukagimhan.ai@gmail.com>
2026-05-03 16:41:49 +0530

    feat: update project data and add new SVG logo for SYNTIOX Store

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,12 +2,40 @@ const syntioxData = {
     projects: [
         {
             id: 1,
-            name: "NexusCore",
-            logo: "NC",
+            name: "SYNTIOX SYNC",
+            logo: "SS",
             logoImg: "",
-            description: "Enterprise grade neural network framework engineered for real-time threat detection and automated response protocols.",
-            technologies: ["Python", "TensorFlow", "Rust", "WebAssembly"],
-            link: "#"
+            description: "A secure WhatsApp session bridge designed for real-time server synchronization. It supports seamless connectivity via QR scans and pairing codes.",
+            technologies: ["JavaScript", "Node.js"],
+            link: "https://syntiox-sync.vercel.app/"
+        },
+                {
+            id: 2,
+            name: "Xync",
+            logo: "xc",
+            logoImg: "",
+            description: "Xync is a real-time chat application inspired by WhatsApp Web. Built with Firebase for instant messaging, secure auth, and smart contact management with a fully responsive UI. 📱💻",
+            technologies: ["TypeScript", "Firebase"],
+            link: "https://xync-psi.vercel.app/"
+        },
+        {
+            id: 3,
+            name: "SYNTIOX Store",
+            logo: "SS",
+            logoImg: "",
+            description: "Fast and reliable game top-up center for Free Fire, PUBG Mobile, and Mobile Legends in Sri Lanka.",
+            technologies: ["HTML5", "CSS3", "JavaScript", "TypeScript"],
+            link: "https://syntiox-diamond-store.vercel.app/"
+        },
+
+        {
+            id: 4,
+            name: "Fashion-Shop",
+            logo: "Fs",
+            logoImg: "",
+            description: "Fashion-Shop-II is a full-stack e-commerce platform with a Node.js backend and responsive frontend, supporting product management via an integrated seller center and JSON storage.",
+            technologies: ["HTML5", "CSS3", "JavaScript"],
+            link: "https://github.com/Syntiox/Fashion-Shop-II"
         }
     ],
     gallery: [
@@ -15,6 +43,7 @@ const syntioxData = {
     ],
     team: [
 
-        { id: 1, name: "Shaluka Gimhan", role: "Founder", image: "https://github.com/sh4lu-z.png", link: "https://sh4lu-z.vercel.app/" }
+        { id: 1, name: "Shaluka Gimhan", role: "Founder", image: "https://github.com/sh4lu-z.png", link: "https://sh4lu-z.vercel.app/" },
+        { id: 2, name: "AI Agent", role: "AI Agent", image: "https://miro.medium.com/1*xTRNH1cab8IsIKL5OWo-uw.jpeg", link: "#" }
     ]
 };
